fb82b0710ef89fd8538f9b5807b3e98737e4a3ba
Nathaniel Miller <dukeoflaser@gmail.com>
2016-04-03 04:34:25 +0000

    Make tests pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,23 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params["username"])
+    # binding.pry
+    if @user.nil? || @user.password != params["password"]
+      erb:'error'
+    else
+      session[:user_id] = @user.id
+      redirect '/account'  
+    end
   end
 
   get '/account' do
-
+    @session = session
+    erb:'account'
   end
 
   get '/logout' do
-
+    session.clear
   end
 
 
